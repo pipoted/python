7192c77e7ab2d873805f9fbd6d59a43e9a31eabf
pipoted <32660879+pipoted@users.noreply.github.com>
2018-10-25 12:07:02 +0800

    nothing

--- 第九章 迭代器与生成器/gen_func.py
+++ 第九章 迭代器与生成器/gen_func.py
@@ -6,6 +6,33 @@
 def gen_func():
     yield 1
     yield 2
+    yield 3
+# 惰性求值，延迟求值
+
+
+def fib(index):
+    if index <= 2:
+        return 1
+    else:
+        return fib(index - 1) + fib(index - 2)
+
+
+def fib2(index):
+    re_list = []
+    n, a, b = 0, 0, 1
+    while n < index:
+        re_list.append(b)
+        a, b = b, a + b
+        n += 1
+    return re_list
+
+
+def gen_fib(index):
+    n, a, b = 0, 0, 1
+    while n < index:
+        yield b
+        a, b = b, a+b
+        n += 1
 
 
 def func():
@@ -14,8 +41,7 @@ def func():
 
 if __name__ == '__main__':
     # 生成器对象,python编译字节码时生成的
-    gen = gen_func()
-    for item in gen:
-        print(item)
-    re = func()
-    pass
+    print(fib(index=10))
+    print(fib2(index=10))
+    for date in gen_fib(index=10):
+        print(date)
